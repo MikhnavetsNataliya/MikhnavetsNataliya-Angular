6b65bbdb12bd7ed83e9502d86381c13b0da56670
Nataliya Mikhnavets <mikhnavets.natali@gmail.com>
2019-10-17 21:53:03 +0300

    Add page contact with form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  @Output()
+  update: EventEmitter<any> = new EventEmitter<any>();
+  name: string;
+  email: string;
+  phone: string;
+  message: string;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  /*processForm() {
+    const infoClient = `Меня зовут ${this.name}. Email ${this.email}. Телефон: ${this.phone}. Сообщение: ${this.message}`;
+    alert(infoClient);
+  }*/
+  onSubmit(isValid: boolean) {
+    const infoClient = `Меня зовут ${this.name}. Email ${this.email}. Телефон: ${this.phone}. Сообщение: ${this.message}`;
+
+  }
 }
